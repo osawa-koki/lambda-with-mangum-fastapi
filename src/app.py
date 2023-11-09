aeeb4ded4e5fa5e75be8df8bbc24b880d121adc6
osawa-koki <koki@osawa.dev>
2023-11-10 01:11:03 +0900

    リファクタリング。

--- src/app.py
+++ src/app.py
@@ -5,18 +5,18 @@ app = FastAPI()
 
 @app.get("/api/ping", status_code=200)
 async def root():
-  return {"message": "Hello GET."}
+    return {"message": "Hello GET."}
 
 @app.post("/api/ping", status_code=200)
 async def root():
-  return {"message": "Hello POST."}
+    return {"message": "Hello POST."}
 
 @app.put("/api/ping", status_code=200)
 async def root():
-  return {"message": "Hello PUT."}
+    return {"message": "Hello PUT."}
 
 @app.delete("/api/ping", status_code=200)
 async def root():
-  return {"message": "Hello DELETE."}
+    return {"message": "Hello DELETE."}
 
 lambda_handler = Mangum(app)
