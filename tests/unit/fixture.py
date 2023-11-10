478d742a3ae7c051ba089cd308c2d9d621c9ccf9
osawa-koki <koki@osawa.dev>
2023-11-10 12:35:45 +0900

    テストケース増やした。

---
+++ tests/unit/fixture.py
@@ -0,0 +1,16 @@
+import pytest
+
+from .const import request_context
+
+@pytest.fixture()
+def apigw_event():
+    """ Generates API GW Event"""
+
+    return {
+        "headers": {},
+        "queryStringParameters": {},
+        "pathParameters": {},
+        "stageVariables": {},
+        "resource": "/{proxy+}",
+        "requestContext": request_context,
+    }
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -4,23 +4,9 @@ import pytest
 
 from src import app
 
-from .const import request_context
+from .fixture import apigw_event
 
-
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
-
-    return {
-        "headers": {},
-        "queryStringParameters": {},
-        "pathParameters": {},
-        "stageVariables": {},
-        "resource": "/{proxy+}",
-        "requestContext": request_context,
-    }
-
-def test_lambda_handler(apigw_event):
+def test_ping_get(apigw_event):
 
     apigw_event["httpMethod"] = "GET"
     apigw_event["path"] = "/api/ping"
@@ -32,3 +18,45 @@ def test_lambda_handler(apigw_event):
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
     assert "GET" in data["message"]
+
+
+def test_ping_post(apigw_event):
+
+    apigw_event["httpMethod"] = "POST"
+    apigw_event["path"] = "/api/ping"
+    apigw_event["body"] = {}
+
+    ret = app.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert "POST" in data["message"]
+
+
+def test_ping_put(apigw_event):
+
+    apigw_event["httpMethod"] = "PUT"
+    apigw_event["path"] = "/api/ping"
+    apigw_event["body"] = {}
+
+    ret = app.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert "PUT" in data["message"]
+
+
+def test_ping_delete(apigw_event):
+
+    apigw_event["httpMethod"] = "DELETE"
+    apigw_event["path"] = "/api/ping"
+    apigw_event["body"] = {}
+
+    ret = app.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert "DELETE" in data["message"]
