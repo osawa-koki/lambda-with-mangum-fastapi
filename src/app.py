dae306b62b53cc656a830b8cf294659affb51736
osawa-koki <koki@osawa.dev>
2023-11-10 23:42:59 +0900

    ローカルで正常に動作するように。

--- src/app.py
+++ src/app.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-from .routers import ping
-
 app = FastAPI()
-app.include_router(ping.router)
+
+
+@app.get("/api/ping", status_code=200)
+async def ping_get():
+    return {"message": "Hello GET."}
+
+
+@app.post("/api/ping", status_code=200)
+async def ping_post():
+    return {"message": "Hello POST."}
+
+
+@app.put("/api/ping", status_code=200)
+async def ping_put():
+    return {"message": "Hello PUT."}
+
+
+@app.delete("/api/ping", status_code=200)
+async def ping_delete():
+    return {"message": "Hello DELETE."}
 
 lambda_handler = Mangum(app)
